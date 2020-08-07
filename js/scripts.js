ca143f7ab839609c3f41620010d6b9315851ca72
alexburh95 <alexburh@gmail.com>
2020-08-07 16:04:42 +0300

    Соло скарточка

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ $(function() {
 	
     var target_block = $(".cifra1"); // Ищем блок 
     var blockStatus = true;
-    var numb_start = $(".cifra1").text() + "";
+    var numb_start = $(".cifra1").text();
     
     $(window).scroll(function() {
     
-        var scrollEvent = ($(window).scrollTop() > (target_block.position().top - $(window).height()));
+       
         
-        if(scrollEvent && blockStatus) {
+        if(blockStatus) {
         
             blockStatus = false; // Запрещаем повторное выполнение функции до следующей перезагрузки страницы.
             
@@ -39,13 +39,13 @@ $(function() {
 	
     var target_block2 = $(".cifra2"); // Ищем блок 
     var blockStatus2 = true;
-    var numb_start2 = $(".cifra2").text() + "";
+    var numb_start2 = $(".cifra2").text();
     
     $(window).scroll(function() {
     
-        var scrollEvent2 = ($(window).scrollTop() > (target_block2.position().top - $(window).height()));
+       
         
-        if(scrollEvent2 && blockStatus2) {
+        if(blockStatus2) {
         
             blockStatus2 = false; // Запрещаем повторное выполнение функции до следующей перезагрузки страницы.
             
